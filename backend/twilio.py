3c47b998bb64bee69e1f8c42d17a25816df63cc9
CoolsomeGamer21 <105989804+CoolsomeGamer21@users.noreply.github.com>
2023-01-07 15:33:10 -0500

    Create twilio.py

---
+++ backend/twilio.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+var recipe = ""
+var numero = ""
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+auth_token  = "your_auth_token"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+15558675309", 
+    from_="+15017250604",
+    body="Hello, you have + $(numero) + $(recipe) + saved ")
+
+print(message.sid)
