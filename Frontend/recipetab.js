460b564878d06f706ead2188db94179933f4c6da
CoolsomeGamer21 <105989804+CoolsomeGamer21@users.noreply.github.com>
2023-01-07 16:36:59 -0500

    Create recipetab.js

---
+++ Frontend/recipetab.js
@@ -0,0 +1,78 @@
+<html>
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sportify</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body style="background-color:#5c2015">
+  <header class="header">
+    <a href="#" class="logo">RecipeCheckr</a>
+    <nav class="nav-items">
+      <a href="index.html">Home</a>
+      <a href="recipes.html">Find Recipes</a>
+      <a href="play_area.html">Saved Recipes</a>
+      <a href="upload.html">Upload a Recipe</a>
+      <a href="my_account.html">My Account</a>
+  </header>
+  
+<h1 style="color:#fff">Check out these Recipes!</h1>
+    <div class="achievements">
+      
+    <div class="recipes">
+      <img src="https://www.simplyrecipes.com/thmb/1qlP36kzpJIkLq3DR10qjINflcI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Easy-Banana-Bread-LEAD-2-66837beab63c495292d89743c6767171.jpg" alt="Fitness graph" width="190" height="165">
+      <h2>Eggless Banana Bread</h2>
+      <ul>
+        <li>Diet: Vegan</li>
+        <li>Total Time: 55 mins</li>
+        <li>Cuisine Type: international</li>
+        <li>Serving: 1 loaf</li>
+        <button onclick="window.location.href = 'recipe 1.html';"> Check out this recipe</button></button>
+      </ul>
+    </div>
+
+    <br>
+    <div class="recipes">
+      <img src= "https://www.allrecipes.com/thmb/gXP5GFBfQJ7ZvzprYxsFuRl5aSc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/693494-extreme-chocolate-cake-Jessica-1x1-1-3ae3c7dc822843b893222a0fb97efba8.jpg" alt="Fitness graph" width="190" height="165">
+      <h2>Chocolate Cake</h2>
+      <ul>
+        <li>Vegan: Yes</li>
+        <li>Food: Desert</li>
+        <li>Cuisine: International</li>
+        <li>YUMMY!!!</li>
+        <button><a href="https://www.samsung.com/us/watches/galaxy-watch5-pro/" target="_blank">Check Out this Recipe</a></button>
+      </ul>
+    </div>
+    
+      
+
+
+      
+    </div>
+        </main>
+      
+    <footer class="footer">
+    <div class="copy">&copy; 2022 PriorityHealth. All Rights Reserved.</div>
+    <div class="bottom-links">
+      <div class="links">
+        <a href="index.html">Home</a>
+      <a href="fitness.html">Fitness</a>
+      <a href="mental_health.html">Mental Health Clinic</a>
+      <a href="upload.html">Connect with Doctor</a>
+      <a href="my_account.html">My Account</a>
+      </div>
+      
+    </div>
+  </footer>
+      </body>
+
+</html>
+
+    
+      
